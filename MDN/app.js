9ac736d465ce02eb30ba2d1b65df483c339fd26b
zep1994 <snaxbags@protonmail.com>
2019-12-05 01:38:29 -0500

    setup mongoose

--- MDN/app.js
+++ MDN/app.js
@@ -1,24 +1,44 @@
+//define dependencies
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+//Import the mongoose module
+var mongoose = require('mongoose');
+
+//Set up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/my_database';
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+
+//define routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//call instance of express function and create our express module
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+//add middleware libraries 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//route-handling
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
